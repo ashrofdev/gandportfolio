e2b2f4d701abd8bde5e21d8e5093a731cef685d1
sammykae <sammykay301@live.com>
2022-02-26 15:13:19 +0100

    clear error

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,49 +1,59 @@
-import React, { useState } from 'react';
-import Skills from './Skills';
+import React from "react";
 
-import fsp from '../img/fsp.png'
-import venue from '../img/venue.png'
-import trello from '../img/trello.png'
+import fsp from "../img/fsp.png";
+import venue from "../img/venue.png";
+import trello from "../img/trello.png";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 
-const Hero = ({openModal}) => {
-   
-    return (
-        <div className="container grid hero">
-            <div className="fullwidth">
-                <div className="content">
-                    <h1 style={{margin:'1rem 0'}}>Need to do something on the web?</h1>
-                    <h3 style={{margin:'1rem 0'}}>You've come to the right place</h3>
-                    <div style={{display:'flex', alignItems:"center"}}>
-                        <p>I am a web engineer with experience working with</p>
-                        <p>HTML</p>
-                    </div>
-                    <div className="cta">
-                        <button onClick={openModal}>Hire now</button>
-                        <a style={{color:'black'}} target="_blank" href="https://drive.google.com/file/d/1dqfrZPjNilMGin8x41IwuhfeNswjPLUq/view?usp=sharing">Download CV &rarr;</a>
-                    </div>
-                </div>
-                <div className="imgshow">
-                    <Carousel autoPlay={true} infiniteLoop={true} showArrows={false} showThumbs={false}>
-                        <div>
-                            <img src={venue} />
-                        </div>
-                        <div>
-                            <img src={fsp} />
-                        </div>
-                        <div>
-                            <img src={trello} />
-                        </div>
-                    </Carousel>
-                </div>
-            </div>
-            {/* <div className="fullwidth">
+const Hero = ({ openModal }) => {
+	return (
+		<div className="container grid hero">
+			<div className="fullwidth">
+				<div className="content">
+					<h1 style={{ margin: "1rem 0" }}>Need to do something on the web?</h1>
+					<h3 style={{ margin: "1rem 0" }}>You've come to the right place</h3>
+					<div style={{ display: "flex", alignItems: "center" }}>
+						<p>I am a web engineer with experience working with</p>
+						<p>HTML</p>
+					</div>
+					<div className="cta">
+						<button onClick={openModal}>Hire now</button>
+						<a
+							style={{ color: "black" }}
+							target="_blank"
+							rel="noreferrer"
+							href="https://drive.google.com/file/d/1dqfrZPjNilMGin8x41IwuhfeNswjPLUq/view?usp=sharing"
+						>
+							Download CV &rarr;
+						</a>
+					</div>
+				</div>
+				<div className="imgshow">
+					<Carousel
+						autoPlay={true}
+						infiniteLoop={true}
+						showArrows={false}
+						showThumbs={false}
+					>
+						<div>
+							<img alt="venue" src={venue} />
+						</div>
+						<div>
+							<img alt="fsp" src={fsp} />
+						</div>
+						<div>
+							<img alt="trello" src={trello} />
+						</div>
+					</Carousel>
+				</div>
+			</div>
+			{/* <div className="fullwidth">
                 <Skills/>
             </div> */}
-        </div>
-    );
+		</div>
+	);
 };
 
 export default Hero;
